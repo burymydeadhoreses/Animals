d9ee11decd370032431c2fb1d15fd864202a9a0f
defectly <rikarsoft@gmail.com>
2024-05-27 09:43:35 +0300

    Add project files.

---
+++ Animals/Animal.cs
@@ -0,0 +1,70 @@
+﻿abstract class Animal(string name, DateTime birthdate, List<string> commands)
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; private set; } = name;
+    public DateTime Birthdate { get; private set; } = birthdate;
+    public List<string> Commands { get; private set; } = commands;
+
+    public static Animal CreateNewAnimal(string type, string name, DateTime birthdate)
+    {
+        return type.ToUpper() switch
+        {
+            "CAT" => new Cat(type, name, birthdate, new List<string>()),
+            "DOG" => new Cat(type, name, birthdate, new List<string>()),
+            "HAMSTER" => new Cat(type, name, birthdate, new List<string>()),
+
+            "HORSE" => new Cat(type, name, birthdate, new List<string>()),
+            "CAMEL" => new Cat(type, name, birthdate, new List<string>()),
+            "DONKEY" => new Cat(type, name, birthdate, new List<string>()),
+
+            _ => throw new ArgumentException("No such animal type")
+        };
+    }
+
+    public virtual void LearnCommand(string command)
+    {
+        Commands.Add(command);
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id},\nName: {Name},\nBirthdate: {Birthdate},\nCommands:\n{string.Join('\n', Commands)}";
+    }
+}
+abstract class Pet(string type, string name, DateTime birthdate, List<string> commands) : Animal(name, birthdate, commands)
+{
+    public string Type { get; private set; } = type;
+
+    public override string ToString()
+    {
+        return $"Type: {Type},\n" + base.ToString();
+    }
+}
+abstract class Pack(string type, string name, DateTime birthdate, List<string> commands) : Animal(name, birthdate, commands)
+{
+    public string Type { get; private set; } = type;
+
+    public override string ToString()
+    {
+        return $"Type: {Type},\n" + base.ToString();
+    }
+}
+
+class Cat(string type, string name, DateTime birthdate, List<string> commands) : Pet(type, name, birthdate, commands)
+{
+}
+class Dog(string type, string name, DateTime birthdate, List<string> commands) : Pet(type, name, birthdate, commands)
+{
+}
+class Hamster(string type, string name, DateTime birthdate, List<string> commands) : Pet(type, name, birthdate, commands)
+{
+}
+class Horse(string type, string name, DateTime birthdate, List<string> commands) : Pack(type, name, birthdate, commands)
+{
+}
+class Camel(string type, string name, DateTime birthdate, List<string> commands) : Pack(type, name, birthdate, commands)
+{
+}
+class Donkey(string type, string name, DateTime birthdate, List<string> commands) : Pack(type, name, birthdate, commands)
+{
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,104 @@
+﻿var cat = Animal.CreateNewAnimal("cat", "Whiskey", DateTime.Now);
+
+Console.WriteLine("meow");
+
+int animalCount = 0;
+List<Animal> animals = new();
+
+GetMenu();
+
+void GetMenu()
+{
+    Console.WriteLine("[1] Add animal");
+    Console.WriteLine("[2] Get commands");
+    Console.WriteLine("[3] Learn command");
+    Console.WriteLine("[4] Get animals by birthdate");
+    Console.WriteLine($"    Animals: {animalCount}");
+
+    Route(Console.ReadKey().Key);
+    Console.WriteLine();
+    ReturnMenu();
+}
+
+void ReturnMenu()
+{
+    Console.WriteLine($"Press enter to show menu");
+
+    while(true)
+    {
+        if(Console.ReadKey().Key == ConsoleKey.Enter)
+        {
+            Console.Clear();
+            GetMenu();
+            break;
+        }
+    }
+}
+
+void Route(ConsoleKey key)
+{
+    Console.Clear();
+
+    switch (key)
+    {
+        case ConsoleKey.D1:
+            AddAnimal();
+            break;
+        case ConsoleKey.D2:
+            Console.WriteLine("Enter id:");
+            GetCommands(Guid.Parse(Console.ReadLine()));
+            break;
+        case ConsoleKey.D3:
+            Console.WriteLine("Enter id, command or empty string for return:");
+            string[] input = Console.ReadLine().Replace(" ", "").Split(",");
+            if (string.IsNullOrEmpty(input[0]))
+            {
+                ReturnMenu();
+                return;
+            }
+            LearnCommand(Guid.Parse(input[0]), input[1]);
+            break;
+        case ConsoleKey.D4:
+            OrderByBirthdate();
+            break;
+
+        default:
+            Console.WriteLine("Unknown input");
+            GetMenu();
+            break;
+    };
+}
+
+void OrderByBirthdate()
+{
+    Console.WriteLine(string.Join('\n', animals.OrderBy(animal => animal.Birthdate).ToList()));
+}
+
+void LearnCommand(Guid Id, string command)
+{
+    animals.Single(animal => animal.Id == Id).Commands.Add(command);
+}
+
+void GetCommands(Guid Id)
+{
+    Console.WriteLine();
+    Console.WriteLine("Commands: ");
+    Console.WriteLine(string.Join('\n', animals.Single(animal => animal.Id == Id).Commands));
+}
+
+void AddAnimal()
+{
+    Console.WriteLine("Enter type, name, birthday:");
+    Console.WriteLine();
+    string[] input = Console.ReadLine().Replace(" ", "").Split(",");
+
+    var animal = Animal.CreateNewAnimal(input[0], input[1], DateTime.Parse(input[2]));
+
+    animals.Add(animal);
+
+    animalCount++;
+
+    Console.WriteLine();
+
+    Console.WriteLine(animal);
+}
